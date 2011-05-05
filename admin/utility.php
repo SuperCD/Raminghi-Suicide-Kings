bbda5d8ea3c8aafd550bf19a9e92b7b52529bc86
Nicola Petracchi <eymerich@gmail.com>
2011-05-06 00:26:51 +0200

    - Aggiunto il file gitignore con una prima tranche di roba inutile. - Spostati i parametri di configurazione negli appositi files ed implementato il bottone di toolbar per accedere alla gestione dei parametri. - Spostata la toolbar lato admin nel file template di default piuttosto che nell'azione controller. - Modificata la classe utility di acquisizione della connessione DB per phpRaider considerando il nuovo sistema di gestione parametri. - Sistemata un po l'indentazione del file XML di installazione ed aggiunti tutti i files che mancavano alla lista per l'installazione.

--- admin/utility.php
+++ admin/utility.php
@@ -2,16 +2,29 @@
 // No direct access
  defined( '_JEXEC' ) or die( 'Restricted access' );
  
- class SKManagerUtilities {
- 	function getRaiderDB () {
+class SKManagerUtilities
+{
+	/**
+	 * Recupera l'istanza di collegamento al database del phpRaider.
+	 * @return mixed istanza di collegamento oppure <strong>JException</strong> se mancano dati/errore.
+	 */
+	function getRaiderDB ()
+	{	
+		$params = &JComponentHelper::getParams('com_skmanager');
+		
 		$option = array(); //prevenire problemi
 
-		$option['driver']   = 'mysql';            // Nome del driver del Database
-		$option['host']     = 'localhost';    // Nome host del Database
-		$option['user']     = 'root';       // Utente per l'autenticazione al Database
-		$option['password'] = '';   // Password
-		$option['database'] = 'phpraider';      // Nome del Database
-		$option['prefix']   = 'phpraider_';             // Prefisso per le tabelle del Database (può essere vuoto)
+		$option['driver']   = $params->get('phpraider_db_driver', NULL);		// Nome del driver del Database
+		$option['host']     = $params->get('phpraider_db_host', NULL);			// Nome host del Database
+		$option['user']     = $params->get('phpraider_db_username', NULL);		// Utente per l'autenticazione al Database
+		$option['password'] = $params->get('phpraider_db_password', NULL);		// Password
+		$option['database'] = $params->get('phpraider_db_dbname', NULL);		// Nome del Database
+		$option['prefix']   = $params->get('phpraider_db_tablesprefix', NULL);	// Prefisso per le tabelle del Database (può essere vuoto)
+		
+		$res = TRUE;
+		foreach($option as $myopt) if(empty($myopt)) $res = FALSE;
+		
+		if(!$res) return new JException(JTEXT::_('Parametri di connessione al database del phpRaider insufficienti.'));
 		return JDatabase::getInstance( $option );
 	}
- }
+}
--- admin/views/skmanagers/view.html.php
+++ admin/views/skmanagers/view.html.php
@@ -5,15 +5,6 @@ jimport( 'joomla.application.component.view' );
 class SKManagersViewSKManagers extends JView {
 	function display($tpl = null)
     {
-        JToolBarHelper::title( JText::_( 'Suicide Kings Manager' ), 'generic.png' );
-        JToolBarHelper::deleteList();
-        JToolBarHelper :: custom( 'suicide', 'iconname.png', 'iconname.png', 'Suicide', false, false );
-        JToolBarHelper :: custom( 'import', 'iconname.png', 'iconname.png', 'Importa giocatori', false, false );
-        JToolBarHelper :: custom( 'raids', 'iconname.png', 'iconname.png', 'Gestione Raid', false, false ); 
-        JToolBarHelper::editListX();
-        JToolBarHelper::addNewX();
- 
-
         $playerModel =& JModel::getInstance('player','SKManagersModel');
  		
         $playerList = $playerModel->getStandingData();
